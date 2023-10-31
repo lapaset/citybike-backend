6fd5a941161064d5df92c2777824fd8dc78a2796
Liisa Lahti <liisa.r.lahti@gmail.com>
2023-10-31 21:31:52 +0200

    Connect to postgres database

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 import express, { Application, Request, Response } from 'express'
 import dotenv from 'dotenv'
+import { query } from './db/index'
 
 dotenv.config()
 
 const app: Application = express()
 const port = process.env.PORT || 3001
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('hello citybike')
+app.get('/', async (req: Request, res: Response) => {
+  const all = await query('SELECT * FROM STATION')
+  res.send(all.rows)
 })
 
 app.listen(port, () => {
