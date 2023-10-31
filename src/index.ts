210e7beda56fa6bc7ba5c015708342c62654c479
Liisa Lahti <liisa.r.lahti@gmail.com>
2023-10-31 20:59:22 +0200

    Configure eslint and prettier

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application, Request, Response } from 'express'
 
-const app: Application = express();
-const port = process.env.PORT || 3001;
+const app: Application = express()
+const port = process.env.PORT || 3001
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("hello citybike");
-});
+app.get('/', (req: Request, res: Response) => {
+  res.send('hello citybike')
+})
 
 app.listen(port, () => {
-  console.log(`Server is live at http://localhost:${port}`);
-});
+  console.log(`Server is live at http://localhost:${port}`)
+})
