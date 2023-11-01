94d9f743fb9b80e12de02e20da5dac5a20bf7355
Liisa Lahti <liisa.r.lahti@gmail.com>
2023-11-01 17:12:04 +0200

    Use cors

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import cors from 'cors'
 import express, { Application } from 'express'
 import dotenv from 'dotenv'
 import mountRoutes from './routes/index'
@@ -5,9 +6,10 @@ import mountRoutes from './routes/index'
 dotenv.config()
 
 const app: Application = express()
-const port = process.env.PORT || 3001
-
+app.use(cors())
 mountRoutes(app)
+
+const port = process.env.PORT || 3001
 
 app.listen(port, () => {
   console.log(`Server is live at http://localhost:${port}`)
