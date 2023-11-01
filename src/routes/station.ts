12afd171bd055e288f2e983fecbe18bd43260c3d
Liisa Lahti <liisa.r.lahti@gmail.com>
2023-11-01 17:13:57 +0200

    Add route for getting single station stats - journeys started from station - jouneysy ended to station - average duration and distance of journeys from station

--- src/routes/station.ts
+++ src/routes/station.ts
@@ -6,7 +6,20 @@ const router = new (Router as any)()
 
 router.get('/', async (req: Request, res: Response) => {
   const previousMax = req.query.previousMax?.toString() || '0'
-  const response = await query('SELECT * FROM STATION WHERE id > $1 ORDER BY id LIMIT 20;', [previousMax])
+  const response = await query('SELECT * FROM STATION WHERE id > $1 ORDER BY id LIMIT 30;', [previousMax])
+  res.send(response.rows)
+})
+
+router.get('/:id', async (req: Request, res: Response) => {
+  const id = req.params.id
+  const response = await query(
+    'SELECT COUNT(*) as start_count, avg(duration) as avg_duration, avg(distance) as avg_distance, (\
+      SELECT COUNT(*) FROM JOURNEY WHERE return_station_id = $1\
+    ) AS end_count\
+     FROM JOURNEY WHERE departure_station_id = $1\
+    ;',
+    [id]
+  )
   res.send(response.rows)
 })
 
